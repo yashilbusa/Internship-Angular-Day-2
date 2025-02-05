c9d59635a156ff7e8746f120417b5cb41680c953
yashilbusa <yashilpatel1914@gmail.com>
2025-02-05 11:51:41 +0530

    commit

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,7 +10,7 @@ export class DataService {
   constructor(private http: HttpClient) { }
   private url = 'https://jsonplaceholder.typicode.com/posts/2'; 
 
-  getPosts(): Observable<any> {
+  getPosts(){
     return this.http.get(this.url);
   }
 }
